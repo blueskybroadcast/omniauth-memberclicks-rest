caacfe161cc31592d07933661e2c1471f91a1d56
Max Grechko <gremax@gremax.me>
2018-07-18 12:38:12 +0300

    Build Memberclicks REST integration strategy

--- lib/omniauth/strategies/memberclicks_rest.rb
+++ lib/omniauth/strategies/memberclicks_rest.rb
@@ -3,6 +3,113 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class MemberclicksREST < OmniAuth::Strategies::OAuth2
+      option :name, 'memberclicks_rest'
+
+      option :client_options, {
+        authorize_url: '/oauth/v1/authorize',
+        site: 'MUST_BE_PROVIDED',
+        token_url: '/oauth/v1/token',
+        user_info_url: '/api/v1/profile/me'
+      }
+
+      uid { info[:uid] }
+
+      info { raw_user_info }
+
+      def request_phase
+        redirect client.auth_code.authorize_url({ redirect_uri: "#{callback_url}?slug=#{account_slug}" }.merge(authorize_params))
+      end
+
+      def callback_phase
+        return fail!(:invalid_credentials) unless request.params['code']
+
+        response = connection.post(options.client_options.token_url) do |request|
+          request.headers['Authorization'] = auth_header
+          request.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+          request.body = auth_body
+        end
+
+        if response.success?
+          prepare_access_token(response.body)
+          self.env['omniauth.origin'] = '/' + account_slug
+          self.env['omniauth.auth'] = auth_hash
+
+          call_app!
+        else
+          fail!(:invalid_credentials)
+        end
+      end
+
+      private
+
+      def account_slug
+        request.params['slug'] || session['omniauth.params']['origin'].gsub(/\//,'')
+      end
+
+      def auth_body
+        {
+          grant_type: 'authorization_code',
+          code: request.params['code'],
+          redirect_uri: "#{callback_url}?slug=#{account_slug}"
+        }.to_param
+      end
+
+      def auth_hash
+        AuthHash.new(provider: name, uid: uid, info: info)
+      end
+
+      def auth_header
+        hash = Base64.encode64(options[:client_id] + ':' + options[:client_secret]).delete("\n")
+        "Basic #{hash}"
+      end
+
+      def connection
+        Faraday.new(url: options.client_options.site) do |request|
+          request.headers['Accept'] = 'application/json'
+          request.adapter(Faraday.default_adapter)
+        end
+      end
+
+      def prepare_access_token(raw_body)
+        response_body = MultiJson.load(raw_body)
+
+        self.access_token = {
+          token: response_body['access_token'],
+          token_expires: response_body['expires_in'],
+          refresh_token: response_body['refresh_token']
+        }
+      end
+
+      def prepare_user_info(raw_body)
+        return @user_info if defined?(@user_info)
+        parsed_body = MultiJson.load(raw_body)
+
+        @user_info = {
+          first_name: parsed_body['[Name | First]'],
+          last_name: parsed_body['[Name | Last]'],
+          email: parsed_body['[Email | Primary]'],
+          username: parsed_body['[Username]'],
+          uid: parsed_body['[Profile ID]'].to_s,
+          member_status: parsed_body['[Member Status]'],
+          member_type: parsed_body['[Member Type]']
+        }
+      end
+
+      def raw_user_info
+        return @raw_user_info if defined?(@raw_user_info)
+
+        response = connection.get(user_info_url) do |request|
+          request.headers['Authorization'] = "Bearer #{access_token[:token]}"
+        end
+
+        return fail!(:invalid_credentials) unless response.success?
+
+        @raw_user_info ||= prepare_user_info(response.body)
+      end
+
+      def user_info_url
+        options.client_options.user_info_url
+      end
     end
   end
 end
--- spec/omniauth/strategies/memberclicks_rest_spec.rb
+++ spec/omniauth/strategies/memberclicks_rest_spec.rb
@@ -1,2 +1,95 @@
 RSpec.describe OmniAuth::Strategies::MemberclicksREST do
+  let(:token_info) { response_fixture('token') }
+  let(:user_info) { response_fixture('me') }
+  let(:parsed_token_info) { MultiJson.load(token_info) }
+
+  subject { described_class.new('app_id', 'secret') }
+
+  describe '#options' do
+    describe '#name' do
+      it { expect(subject.options.name).to eq('memberclicks_rest') }
+    end
+
+    describe '#client_options' do
+      describe '#authorize_url' do
+        it { expect(subject.options.client_options.authorize_url).to eq('/oauth/v1/authorize') }
+      end
+
+      describe '#site' do
+        it { expect(subject.options.client_options.site).to eq('MUST_BE_PROVIDED') }
+      end
+
+      describe '#token_url' do
+        it { expect(subject.options.client_options.token_url).to eq('/oauth/v1/token') }
+      end
+
+      describe '#user_info_url' do
+        it { expect(subject.options.client_options.user_info_url).to eq('/api/v1/profile/me') }
+      end
+    end
+  end
+
+  describe '#info' do
+    let(:access_token) do
+      {
+        token: parsed_token_info['access_token'],
+        token_expires: parsed_token_info['expires_in'],
+        refresh_token: parsed_token_info['refresh_token']
+      }
+    end
+
+    before do
+      allow(subject).to receive(:access_token).and_return(access_token)
+      subject.options.client_options[:site] = 'https://org_id.memberclicks.net'
+      stub_request(:get, 'https://org_id.memberclicks.net/api/v1/profile/me')
+        .with(headers: { 'Accept' => 'application/json', 'Authorization' => "Bearer #{parsed_token_info['access_token']}" })
+        .to_return(status: 200, body: user_info, headers: {})
+    end
+
+    context 'first_name' do
+      it 'returns first_name' do
+        expect(subject.info[:first_name]).to eq 'Lori'
+      end
+    end
+
+    context 'last_name' do
+      it 'returns last_name' do
+        expect(subject.info[:last_name]).to eq 'Smith'
+      end
+    end
+
+    context 'email' do
+      it 'returns email' do
+        expect(subject.info[:email]).to eq 'lori.smith.work@memberclicks.com'
+      end
+    end
+
+    context 'username' do
+      it 'returns username' do
+        expect(subject.info[:username]).to eq 'lsmith1o'
+      end
+    end
+
+    context 'uid' do
+      it 'returns uid as string' do
+        expect(subject.info[:uid]).to eq '1001398965'
+      end
+    end
+
+    context 'member_status' do
+      it 'returns member_status' do
+        expect(subject.info[:member_status]).to eq 'Active'
+      end
+    end
+
+    context 'member_type' do
+      it 'returns member_type' do
+        expect(subject.info[:member_type]).to eq 'Associate'
+      end
+    end
+  end
+end
+
+def response_fixture(filename)
+  IO.read("spec/fixtures/#{filename}.json")
 end
